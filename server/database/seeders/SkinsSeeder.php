1cd99ab760afb97fc0ce724f7612c3efd7e2cf38
leandrita <lea.bujh@gmail.com>
2023-11-02 11:03:05 +0100

    create skins seeder

---
+++ server/database/seeders/SkinsSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SkinsSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $skins = [
+            [
+                'name' => 'Thor',
+                'type' => 'Strength',
+                'price' => '50 euros',
+                'color' => 'red',
+                'user_id' => 2,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Loki',
+                'type' => 'Intelligence',
+                'price' => '150 euros',
+                'color' => 'green',
+                'user_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ];
+
+        DB::table('skins')->insert($skins);
+    }
+}
