2d10ae63a04d1587f44bc6bc8722b0885956905e
leandrita <lea.bujh@gmail.com>
2023-11-02 11:08:40 +0100

    create auth controller and update models

---
+++ server/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\UserProfile;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:191',
+            'email' => 'required|email|max:191|unique:users,email',
+            'password' => 'required|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'validation_errors' => $validator->messages(),
+            ]);
+        } else {
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            $token = $user->createToken($user->email . '_Token')->plainTextToken;
+
+            return response()->json([
+                'status' => 200,
+                'username' => $user->name,
+                'token' => $token,
+                'id' => $user->id,
+                'message' => 'Usuario creado correctamente!',
+            ], 201);
+        }
+    }
+
+    public function login(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|max:191',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'validation_errors' => $validator->messages(),
+            ]);
+        } else {
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                return response()->json([
+                    'status' => 401,
+                    'message' => 'Credenciales incorrectas',
+                ]);
+            } else {
+                $token = $user->createToken($user->email . '_Token')->plainTextToken;
+
+                return response()->json([
+                    'status' => 200,
+                    'username' => $user->name,
+                    'token' => $token,
+                    'id' => $user->id,
+                    'message' => 'Se ha iniciado sesión correctamente!',
+                ]);
+            }
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Se ha cerrado la sesión!',
+        ], 200);
+    }
+}
--- server/app/Models/Skin.php
+++ server/app/Models/Skin.php
@@ -21,4 +21,9 @@ class Skin extends Model
         'created_at',
         'updated_at',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
