315e9e3ede7da0adb59b4a0ef9b01a5c012243bf
leandrita <lea.bujh@gmail.com>
2023-11-02 10:40:12 +0100

    create routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,19 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\SkinController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/skins', [SkinController::class, 'index']);
+Route::post('/skin', [SkinController::class, 'store']);
+Route::get('/skin/{id}', [SkinController::class, 'show']);
+Route::post('/skin/{id}', [SkinController::class, 'update']);
+Route::delete('/skin/{id}', [SkinController::class, 'destroy']);
