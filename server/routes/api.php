765833648f6cdb935f328bd0baa24d61a2292303
leandrita <lea.bujh@gmail.com>
2023-11-02 11:11:12 +0100

    update routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,11 +1,22 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\SkinController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::get('/skins', [SkinController::class, 'index']);
-Route::post('/skin', [SkinController::class, 'store']);
-Route::get('/skin/{id}', [SkinController::class, 'show']);
-Route::post('/skin/{id}', [SkinController::class, 'update']);
-Route::delete('/skin/{id}', [SkinController::class, 'destroy']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::post('/skin', [SkinController::class, 'store']);
+    Route::get('/skin/{id}', [SkinController::class, 'show']);
+    Route::post('/skin/{id}', [SkinController::class, 'update']);
+    Route::delete('/skin/{id}', [SkinController::class, 'destroy']);
+});
