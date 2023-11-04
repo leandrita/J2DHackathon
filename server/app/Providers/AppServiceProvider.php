aa3383628e76319cb320c4ba9eb1ac6b99996f24
leandrita <lea.bujh@gmail.com>
2023-11-04 14:11:40 +0100

    modified:   server/app/Providers/AppServiceProvider.php 	renamed:    skins.json -> server/skins.json

--- server/app/Providers/AppServiceProvider.php
+++ server/app/Providers/AppServiceProvider.php
@@ -2,23 +2,31 @@
 
 namespace App\Providers;
 
+use App\Models\Skin;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         // acá deberia tener una función que lea un archivo json (skins.json) y lo guarde en la base de datos
+
+        // print "Este es un mensaje informativo.";
+        // $jsonContent = file_get_contents('skins.json');
+        // $skinsData = json_decode($jsonContent, true);
+
+
+
+        // foreach ($skinsData as $skinData) {
+        //     Skin::updateOrCreate(
+        //         ['id' => $skinData['id'], 'name' => $skinData['name'], 'type' => $skinData['type'], 'price' => $skinData['price'], 'color' => $skinData['color']],
+        //     );
+        // }
     }
 }
